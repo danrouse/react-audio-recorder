c169f90870e92fe7cd0121f0530a484fdac79129
Dan Rouse <dan@rouse.house>
2017-04-04 13:24:09 -0400

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   entry: './dist/AudioRecorder.js',
   output: {
-    filename: './dist/AudioRecorder.min.js'
+    filename: './dist/AudioRecorder.min.js',
+    library: 'AudioRecorder',
+    libraryTarget: 'var'
   },
   externals: {
     react: 'React',
